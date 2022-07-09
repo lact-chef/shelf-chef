850dad6b81d347578ba5bf759b2719ecfa497960
alberto londono <albertolondono.londono5@gmail.com>
2022-07-09 13:12:31 -0500

    Push to test heroku status

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
+// hi 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
