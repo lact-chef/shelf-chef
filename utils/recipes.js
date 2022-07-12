0869adfc4a38e31b4d7c5bfa907ecb8523665e36
alberto londono <albertolondono.londono5@gmail.com>
2022-07-12 03:29:35 -0500

    All up to date code from js branch. 7/12

--- utils/recipes.js
+++ utils/recipes.js
@@ -1,8 +1,7 @@
+const apiKey = '588a41dee0c440b0a634e47c06f82f6f';
 function getRecipes(ingredients) {
-    //   event.preventDefault();
-      const apiKey = '588a41dee0c440b0a634e47c06f82f6f';
   
-      var api = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredients}`;
+      let api = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredients}`;
   
       fetch(api)
           .then((response) => response.json())
