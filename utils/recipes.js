7cb6ffbed3410d44f6590c374de948b1dd4acbb0
alberto londono <albertolondono.londono5@gmail.com>
2022-07-12 03:32:53 -0500

    Create for loop to iterate through data response. Create divs and images for temporary filler before views

--- utils/recipes.js
+++ utils/recipes.js
@@ -1,11 +1,30 @@
 const apiKey = '588a41dee0c440b0a634e47c06f82f6f';
+
 function getRecipes(ingredients) {
   
       let api = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredients}`;
   
       fetch(api)
+      
           .then((response) => response.json())
           .then((data) => {
           console.log(data);
+
+          for (let i = 0; i < data.length; i++) {
+            let { id } = data[i];
+            let { image } = data[i];
+            let { title } = data[i];
+
+            let createDiv = document.createElement("div");
+            let createImg = document.createElement("img");
+             createImg.setAttribute("src", image);
+
+             let createP = document.createElement("p");
+             createP.textContent = title;
+
+             createDiv.appendChild(createP);
+             createDiv.appendChild(createImg);
+             imageBoard.appendChild(createDiv);
+          }
       });
   }
