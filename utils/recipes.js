00dd18d58a2e391aef51f9b02576989295b8abe3
alberto londono <albertolondono.londono5@gmail.com>
2022-07-12 04:33:23 -0500

    Modify file with getInfo function. Able to retrieve data. Add index attribute to div dynamic creation

--- utils/recipes.js
+++ utils/recipes.js
@@ -1,13 +1,17 @@
+let recipeBoard = document.getElementById('recipe-container');
+
 const apiKey = '588a41dee0c440b0a634e47c06f82f6f';
 
 function getRecipes(ingredients) {
-  
-      let api = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredients}`;
-  
-      fetch(api)
 
-          .then((response) => response.json())
-          .then((data) => {
+    // clear board to prevent stacking of old searches 
+    recipeBoard.innerHTML = "";
+
+      let api = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredients}`;
+      
+      fetch(api)
+        .then((response) => response.json())
+        .then((data) => {
           console.log(data);
 
           for (let i = 0; i < data.length; i++) {
@@ -16,31 +20,56 @@ function getRecipes(ingredients) {
             let { title } = data[i];
 
             let createDiv = document.createElement("div");
+            createDiv.setAttribute("data-index", id);
+            
             let createImg = document.createElement("img");
              createImg.setAttribute("src", image);
+             
 
              let createP = document.createElement("p");
              createP.textContent = title;
 
              createDiv.appendChild(createP);
              createDiv.appendChild(createImg);
-             imageBoard.appendChild(createDiv);
+             recipeBoard.appendChild(createDiv);
+             idArray.push(id)
           }
-      });
-
-      getInfo(id)
-  }
-
-function getInfo(id) {
-
-    let api = `https://api.spoonacular.com/recipes/${id}/information`;
-            
-        fetch(api)
-            .then((response) => response.json())
-            .then((data) => {
-                for (let i = 0; i < data.length; i++){
-                    
-                }
-            })
+          
+        });
     
+ 
+      
     }
+
+let idArray = [];
+
+recipeBoard.addEventListener("click", (event) => {
+    getInfo(event);
+})
+
+function getInfo(event) {
+
+    let element = event.target;
+    let imgIndex = element.parentElement.getAttribute("data-index");
+console.log(imgIndex);
+  // this for loop will dynamically create li's 
+  for (let i = 0; i < idArray.length; i++) {
+    
+    let id = idArray[i];
+    console.log(id);
+
+    if (imgIndex == id) {
+        console.log('hi');
+
+        let api = `https://api.spoonacular.com/recipes/${imgIndex}/information?apiKey=${apiKey}`;
+        
+    fetch(api)
+        .then((response) => response.json())
+        .then( (data) => {
+                console.log(data);
+            
+        })
+        
+    }
+  }
+}
