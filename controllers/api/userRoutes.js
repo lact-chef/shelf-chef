c2565591728d213bd72affb7cff44ae871f34391
alberto londono <albertolondono.londono5@gmail.com>
2022-07-16 10:25:49 -0500

    Modify userRoutes.js with correct naming conventions. Remove bcrypt from this file

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,19 +1,7 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
-const { User } = require('../../models/User');
+const { User } = require('../../models');
 
 //Creates a new user, hashes password, and saves to DB
-router.post('/', async (req, res) => {
-  try {
-    const newUser = req.body;
-    newUser.password = await bcrypt.hash(req.body.password, 10);
-    const userData = await User.create(newUser);
-    res.status(200).json(userData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -28,6 +16,7 @@ router.post('/', async (req, res) => {
     res.status(400).json(err);
   }
 });
+
 
 router.post('/login', async (req, res) => {
   try {
