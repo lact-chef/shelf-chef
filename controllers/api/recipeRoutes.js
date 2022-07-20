a59e717fce764380e13d6588d6ef745639314aaa
alberto londono <albertolondono.londono5@gmail.com>
2022-07-20 13:47:00 -0500

    Modify error response

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -8,7 +8,7 @@ router.post('/', async (req, res) => {
       const favoriteData = await Favorites.create(req.body);
       res.status(200).json(favoriteData);
     } catch (err) {
-      res.status(400).json(err);
+      res.status(404).json(err);
     }
   });
 
