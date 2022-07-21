39ed73b363e870e5d7612d10385a031fc03595d7
alberto londono <albertolondono.londono5@gmail.com>
2022-07-21 15:43:08 -0500

    Modify getRecipes.js styling. Modify styling of recipe and favorite handlebar

--- public/js/getRecipes.js
+++ public/js/getRecipes.js
@@ -25,20 +25,23 @@ const getRecipes = async (ingredients) => {
             let createDiv = document.createElement("div");
         // this is the recipe id 
         createDiv.setAttribute("data-index", id);
+        createDiv.setAttribute("class", "bg-red-500 m-7");
         
         
         //use on click locally but make it async before pushing
         let createBtn = document.createElement("button");
         createBtn.setAttribute("onclick", "favorite(event)");
-        createBtn.setAttribute("class", "favorite-button")
+        createBtn.setAttribute("class", "favorite-button flex items-center shadow bg-red-700 px-4 py-2 text-white hover:bg-red-900 m-3")
         createBtn.textContent = "Save"
         
         //use on click locally but make it async before pushing
         let createImg = document.createElement("img");
+        createImg.setAttribute("class", "w-auto m-3 p-3")
         createImg.setAttribute("onclick", "getInfo(event)");
         createImg.setAttribute("src", image);
 
         let createP = document.createElement("p");
+        createP.setAttribute("class", "flex-auto text-lg font-bold m-3")
         createP.textContent = title;
 
         createDiv.appendChild(createP);
