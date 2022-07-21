af54f5c2e9d6ffe6bc1e9c2d07b6a5eb137f6c19
alberto londono <albertolondono.londono5@gmail.com>
2022-07-21 17:18:48 -0500

    Modify stlying of main.handlebars main tag. Modify styling of dynamically created elements in favoriteIdInfo.js and its respective handlebars

--- public/js/favoriteIdInfo.js
+++ public/js/favoriteIdInfo.js
@@ -19,10 +19,14 @@ const getFavIdInfo = async (event) => {
 
             // to hold ingredients
             let createUl = document.createElement("ul");
+            createUl.setAttribute("class", "font-bold m-7 p-7")
+            createUl.textContent = "Ingredients:"
 
             // to hold instructions
             let createOL = document.createElement("ol");
-          
+            createOL.setAttribute("class", "font-bold m-7 p-7")
+            createOL.textContent = "Instructions:"
+
             //loop through ingredients
         for (let j = 0; j < data.extendedIngredients.length; j++){
 
@@ -44,13 +48,15 @@ const getFavIdInfo = async (event) => {
             let recipeInfo = document.getElementById("recipe-info");
 
             let recipeCard = document.createElement("div");
-            
+            recipeCard.setAttribute("class", "m-7 p-7")
+
             recipeCard.appendChild(createSummary);
             recipeCard.appendChild(createUl);
             recipeCard.appendChild(createOL);
             recipeInfo.appendChild(recipeCard);
             
           }); 
+          element.remove()
   }
 
   document
